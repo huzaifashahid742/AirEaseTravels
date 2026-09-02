48ecb7d1c8b13c74eaccab4bc815577a3102213f
huzaifashahid742 <huzaifashahid742@gmail.com>
2026-09-02 16:34:55 +0500

    update

--- src/Components/ClientSide/Marquee.js
+++ src/Components/ClientSide/Marquee.js
@@ -3,8 +3,27 @@ import "../../Css_Folder/Marquee.css";
 
 const Marquee = () => {
   const message = "Welcome to AirEase Travels & Tours • Premium Travel Deals • Expert Student Consultation Facility • ";
+  const message1 = "---- Right now this webiste is in testing phase ----• ";
   return (
+    
     <div className="marquee-container">
+      <div className="marquee-content">
+        {/* Track 1 */}
+        <div className="marquee-track">
+          <span>{message1}</span>
+          <span>{message1}</span>
+          <span>{message1}</span>
+          <span>{message1}</span>
+        </div>
+        {/* Track 2 (Chaser) */}
+        <div className="marquee-track" aria-hidden="true">
+          <span>{message1}</span>
+          <span>{message1}</span>
+          <span>{message1}</span>
+          <span>{message1}</span>
+        </div>
+      </div>
+
       <div className="marquee-content">
         {/* Track 1 */}
         <div className="marquee-track">
